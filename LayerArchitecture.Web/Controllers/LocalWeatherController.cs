e1df83515d041d60417eb51dfa152f57ae018f30
Rouelle Vivant <vivant.rouelle@addactis.com>
2024-06-18 12:07:19 +0200

    feat: test controleur on web layer

---
+++ LayerArchitecture.Web/Controllers/LocalWeatherController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayerArchitecture.Web.Controllers
+{
+    [Route("api/weather/{id}")]
+    [ApiController]
+    public class LocalWeatherController : ControllerBase
+    {
+        [HttpGet("temperature")]
+        public IActionResult GetTemperature(string id)
+        {
+            if (id == "Lyon")
+            {
+                return Ok("30°C");
+            }
+            else if (id == "Angers")
+            {
+                return Ok("25°C");
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+        }
+
+        [HttpGet("rainfall")]
+        public IActionResult GetRainFall(string id)
+        {
+            if (id == "Lyon")
+            {
+                return Ok("0mm");
+            }
+            else if (id == "Angers")
+            {
+                return Ok("30mm");
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+        }
+    }
+}
---
+++ LayerArchitecture.Web/Controllers/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayerArchitecture.Web.Controllers
+{
+    [Route("api/weather")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(string), 200)]
+        public IActionResult Get()
+        {
+            return Ok("Sunny");
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(void), 404)]
+        public IActionResult Get(string id)
+        {
+            if (id == "Lyon")
+            {
+                return Ok("Sunny");
+            }
+            else if (id == "Angers")
+            {
+                return Ok("Cloud");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
